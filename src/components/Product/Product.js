157d48176a3c746d79294791b252049dc3b6a370
saurav ghosh <josesaurav@gmail.com>
2021-03-16 14:39:14 +0600

    new feathers updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -15,7 +15,7 @@ const Product = (props) => {
                 <h4 className="name"><Link to={"/product/" + key}>{name}</Link></h4>
                 <p>by: <b>{seller}</b></p>
                 <p className="price">${price}</p>
-                <p><small>only {stock} left in stock order-soon</small></p>
+                <p><small>only {stock} left in stock order-sooon</small></p>
                 {props.showAddToCart && <button className="button"
                     onClick={() => props.handleProductAdd(props.product)}>
                     <FontAwesomeIcon icon={faCartPlus} /> add to cart</button>}
